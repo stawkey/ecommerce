3ec4e82232353eee92dbc6df0428470e64ba5f9e
stawkey <82365208+stawkey@users.noreply.github.com>
2025-01-03 21:30:28 +0100

    refactor pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
 
 const App = () => (
---
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import styles from "./LoginPage.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import api from "../../utils/api";
+import Navbar from "../../components/Navbar/Navbar";
+
+const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [showMessage, setShowMessage] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await api.post(
+                "/auth/login",
+                { email, password },
+                { headers: { "Content-Type": "application/json" } }
+            );
+
+            setMessage(response.data.message);
+
+            if (response.status === 200) {
+                navigate("/");
+            }
+        } catch (error) {
+            setMessage(error.response?.data?.message || "Login failed");
+        }
+
+        setShowMessage(true);
+        setTimeout(() => setShowMessage(false), 3000);
+    };
+
+    const toggleShowPassword = () => {
+        setShowPassword((prev) => !prev);
+    };
+
+    return (
+        <>
+            <Navbar />
+            <div className={styles.loginWrapper}>
+                {showMessage && <div className={styles.messageBox}>{message}</div>}
+                <div className={styles.loginContainer}>
+                    <h2>Login</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className={styles.inputLabel}>
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="text"
+                                id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className={styles.inputLabel}>
+                            <label htmlFor="password">Password</label>
+                            <div className={styles.passwordInputWrapper}>
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    id="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                                <button
+                                    type="button"
+                                    onClick={toggleShowPassword}
+                                    className={styles.showPasswordBtn}
+                                >
+                                    {showPassword ? (
+                                        <FontAwesomeIcon icon={faEye} />
+                                    ) : (
+                                        <FontAwesomeIcon icon={faEyeSlash} />
+                                    )}
+                                </button>
+                            </div>
+                        </div>
+
+                        <button type="submit">Login</button>
+                    </form>
+                    <Link to="/register">Don't have an account yet?</Link>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default LoginPage;
